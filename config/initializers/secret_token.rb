671b5e2702aba1bb1440cc19ba53f1e5a6f38dc4
jobacca <leerstring@gmail.com>
2013-07-10 15:23:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joblogca::Application.config.secret_token = 'db7378756019107c91cef639ca6af1ef24d2564926d9cee6880861a09e3efdc243d45af1a92eee4a6ffc78178d0923788e71d2f53299e084ae60a7fe1bbc5a8e'
